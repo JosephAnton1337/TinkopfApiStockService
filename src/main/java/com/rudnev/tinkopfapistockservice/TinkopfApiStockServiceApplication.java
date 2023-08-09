8a74cd857d97de70f5a410225be937aa6ead9ec5
Pavel Rudnev <prudnev@ippon.fr>
2023-08-09 22:20:03 +0300

    Initial commit

---
+++ src/main/java/com/rudnev/tinkopfapistockservice/TinkopfApiStockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rudnev.tinkopfapistockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkopfApiStockServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinkopfApiStockServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rudnev/tinkopfapistockservice/TinkopfApiStockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudnev.tinkopfapistockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinkopfApiStockServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
