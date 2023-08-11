cfb9ecaee5f8c9b017c9e3b0e2b8b0d3da7c221f
Pavel Rudnev <prudnev@ippon.fr>
2023-08-11 17:21:23 +0300

    Cosmetic changes

--- src/main/java/com/rudnev/tinkopfapistockservice/TinkoffApiStockServiceApplication.java
+++ src/main/java/com/rudnev/tinkopfapistockservice/TinkoffApiStockServiceApplication.java
@@ -1,12 +1,10 @@
 package com.rudnev.tinkopfapistockservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableAsync
 public class TinkoffApiStockServiceApplication {
 
